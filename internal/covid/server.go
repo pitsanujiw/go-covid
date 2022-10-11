6a64eda449be16a355dedb53002b69a7f8899cb2
Pitsanu L <pitsanu@deeple.ai>
2022-10-11 17:53:50 +0700

    feat(serv): add server

---
+++ internal/covid/server.go
@@ -0,0 +1,31 @@
+package covidserv
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/pitsanujiw/go-covid/internal/covid/handler"
+	"github.com/pitsanujiw/go-covid/internal/covid/service"
+	"github.com/pitsanujiw/go-covid/pkg/httpclient"
+)
+
+func setup() *gin.Engine {
+	router := gin.New()
+
+	router.Use(gin.Logger(), gin.Recovery())
+
+	http := httpclient.New()
+
+	covidSer := service.NewCovidServ(http)
+
+	covidRoute := router.Group("/covid")
+
+	handler.New(covidRoute, covidSer)
+
+	return router
+}
+
+func Run() {
+	runner := setup()
+
+	runner.Run(":5000")
+}
